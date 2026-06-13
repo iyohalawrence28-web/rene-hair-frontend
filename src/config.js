69bb68005f8c6e68552df7a2393372eca86a8372
iyohalawrence28-web <iyohalawrence28@gmail.com>
2026-06-13 03:04:50 -0700

    add ngrok header bypass

--- src/config.js
+++ src/config.js
@@ -1,1 +1,11 @@
 export const API_BASE = "https://glitter-follicle-fiction.ngrok-free.dev";
+
+export const fetchWithNgrok = (url, options = {}) => {
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      "ngrok-skip-browser-warning": "true",
+    },
+  });
+};
