9445391b2572ff0c3425db829671021b2ff9dee9
iyohalawrence28-web <iyohalawrence28@gmail.com>
2026-06-13 14:23:44 -0700

    force update API_BASE to ngrok

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
-export const API_BASE = "https://rene-hair-backend.onrender.com";
+export const API_BASE = "https://glitter-follicle-fiction.ngrok-free.dev";
 
 export const apiFetch = (url, options = {}) => {
   return fetch(url, {
     ...options,
     headers: {
+      "ngrok-skip-browser-warning": "true",
       ...options.headers,
     },
   });
